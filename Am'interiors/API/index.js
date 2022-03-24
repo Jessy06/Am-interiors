7df98f1299b995e62eb8031da9140072aa67d18b
Jessy06 <91874899+Jessy06@users.noreply.github.com>
2022-03-24 15:57:09 +0100

    Connect DB
    Connexion to db

--- Am'interiors/API/index.js
+++ Am'interiors/API/index.js
@@ -1,27 +1,33 @@
-const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Admin = require('./models/Admin');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
 
-// Database Name
-const dbName = 'Am\'interiors';
+const AdminModel = require("./models/Admin");
+app.use(express.json());
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
 
-  // the following code examples can be pasted here...
-
-  return 'done.';
+// CONNECT TO DB
+mongoose.connect("mongodb+srv://Jessy06:JessyJesse06@cluster0.ppxrt.mongodb.net/Aminteriors?retryWrites=true&w=majority",
+{
+useNewUrlParser : true,
 }
+);
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+
+
+// ROUTES
+app.get('/', async (req, res) => {
+    const user = new AdminModel ({adminname: 'Joshua', nom: 'Delavigne', prenom: 'Joshua', email: 'joshuadel15@gmail.com', password:'123456789'})
+
+    try {
+        await Admin.save ();
+        res.send("Data inserted")
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+// PORT
+app.listen(3030);
