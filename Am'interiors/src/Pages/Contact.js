459e7b9980a5c6f1c48a5fd0310e2ec6bf31cef0
JeremieG06 <91867175+JeremieG06@users.noreply.github.com>
2022-04-06 16:04:47 +0200

    Home.js

--- Am'interiors/src/Pages/Contact.js
+++ Am'interiors/src/Pages/Contact.js
@@ -81,7 +81,7 @@ function Contact() {
         <div class="md:flex md:items-center">
           <div class="md:w-1/3">
             <button
-              class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              class="shadow bg-lime-800 hover:bg-lime-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
               type="button"
             >
               Send
--- Am'interiors/src/Pages/Home.js
+++ Am'interiors/src/Pages/Home.js
@@ -4,8 +4,20 @@ import '../App.css';
 
 function Home () {
     return  (
-        <div>
-            <h1> BIENVENUE </h1>
+        <div className='p-10'>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <p class="text-7xl">AM'INTERIORS</p>
+
+            <br></br>
+
+            <p className="text-2xl">Société<br></br>
+            d'aménagement<br></br>
+            d'intérieurs</p>
+
+
 
          
         </div>
