5a8dc8ca2369c2883970012d5db6aec92115cb53
Jessy06 <91874899+Jessy06@users.noreply.github.com>
2022-04-07 16:39:47 +0200

    Update color navbar
    Same color for the Navbar as the website.
    Border bottom white added to make a separation between the website & the NavBar

--- Am'interiors/src/Pages/Inspirations.js
+++ Am'interiors/src/Pages/Inspirations.js
@@ -7,7 +7,7 @@ import '../App.css';
 function Inspirations () {
     return  (
         <div>
-            <h1> Alors alors ... c'est qui le patron ! </h1>
+            <h1> Alors alors ... c'est qui le patron ! C'est pas toi </h1>
 
          
         </div>
