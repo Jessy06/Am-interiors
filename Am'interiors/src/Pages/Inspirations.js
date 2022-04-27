077fe5674c1b577f78172f147494e487dce07d58
JeremieG06 <91867175+JeremieG06@users.noreply.github.com>
2022-04-27 18:17:40 +0200

    Inspirations.js

--- Am'interiors/src/Pages/Inspirations.js
+++ Am'interiors/src/Pages/Inspirations.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../App.css'
 import Formulaire from '../components/PopUp/Formulaire'
-import Background1 from '../Images/Background1.jpg'
-import Background2 from '../Images/Background2.jpg'
-import Floral from '../Images/Floral.jpg'
-import Rose from '../Images/Rose.jpg'
+import Peinture from '../Images/Peinture.jpg'
 import Thème1 from '../components/Inspirations/Thème1'
+import Thème2 from '../components/Inspirations/Thème2'
+import Thème3 from '../components/Inspirations/Thème3'
+import Thème4 from '../components/Inspirations/Thème4'
+
+
 function Inspirations() {
   return (
     <div className="p-5 pl-5 pr-5 mb-5">
@@ -36,45 +38,70 @@ function Inspirations() {
           commodo. Suspendisse odio magna, pellentesque vel massa eu, malesuada.
         </span>
       </div>
+      <br></br>
+      <br></br>
 
       <div class="flex space-x-4 ">
-          
-        <div className="2xl p-10 pr-25 pb-0.5 pl-0.5 rounded-xl">
-          
-          Thème 1<br></br>
           <br></br>
-          <img src={Background1} className="" alt=''></img>
-          <Thème1/>
-          <br></br>
-          <br></br>
-          <div className="text-xl p-1 pr-1 pb-1 pl-0.5">Explications</div>
-        </div>
-        <div className="2xl p-10 pr-25 pb-0.5 pl-0.5">
-          Théme 2<br></br>
-          <br></br>
-          <img src={Background2} className="" alt=''></img>
-          <br></br>
-          <br></br>
-          <div className="text-xl p-1 pr-1 pb-1 pl-1">Explications</div>
-        </div>
-        <div className="2xl p-10 pr-25 pb-0.5 pl-0.5">
-          Thème 3<br></br>
-          <br></br>
-          <img src={Floral} className="" alt=''></img>
-          <br></br> 
-          <br></br>
-          <div className="text-xl p-1 pr-1 pb-1 pl-1">Explications</div>
-        </div>
-        <div className="2xl p-10 pr-25 pb-0.5 pl-0.5">
-          Thème 4<br></br>
-          <br></br>
-          <img src={Rose} className="" alt=''></img>
-          <br></br>
-          <br></br>
-          <div className="text-xl p-1 pr-1 pb-1 pl-1">Explications</div>
-        </div>
+        <div className="text-2xl">
+        <br></br>
+        <br></br>
       </div>
-    </div>
+      <img src={Peinture} className="" alt='Peinture'></img>
+          <br></br>
+          <br></br>
+          <span className="">
+           <div className='text-3xl'></div>Thème 1 
+            <br></br>
+            <br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis
+          imperdiet quam, eu commodo justo volutpat et. Pellentesque vel
+          tincidunt mauris, porta feugiat odio. Quisque dui tellus, lobortis vel
+          dui id, sollicitudin mollis ligula. Praesent aliquam auctor erat at
+          semper. Cras non euismod odio. Suspendisse et facilisis diam. Mauris a
+          neque tempor, mattis urna vel, tempus sapien. In id quam mattis,
+          tincidunt erat sit amet, hendrerit justo. Maecenas ut aliquet nisi, id
+          congue quam at el Vestibulum sit amet purus nunc. Maecenas vitae
+          imperdiet ex, tempor al accumsan justo. Quisque gravida est et mollis
+          commodo. Suspendisse odio magna, pellentesque vel massa eu, malesuada.
+          <br></br>
+          <br></br>
+          <Thème1/>
+          
+        </span>
+        </div>
+        <br></br>
+        <div class="flex space-x-4 ">
+          <br></br>
+        <div className="text-2xl">
+        <br></br>
+        <br></br>
+      </div>
+          <br></br>
+          <br></br>
+          <span className="">
+           <div className='text-3xl'></div>Thème 2
+            <br></br>
+            <br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis
+          imperdiet quam, eu commodo justo volutpat et. Pellentesque vel
+          tincidunt mauris, porta feugiat odio. Quisque dui tellus, lobortis vel
+          dui id, sollicitudin mollis ligula. Praesent aliquam auctor erat at
+          semper. Cras non euismod odio. Suspendisse et facilisis diam. Mauris a
+          neque tempor, mattis urna vel, tempus sapien. In id quam mattis,
+          tincidunt erat sit amet, hendrerit justo. Maecenas ut aliquet nisi, id
+          congue quam at el Vestibulum sit amet purus nunc. Maecenas vitae
+          imperdiet ex, tempor al accumsan justo. Quisque gravida est et mollis
+          commodo. Suspendisse odio magna, pellentesque vel massa eu, malesuada.
+          <br></br>
+          <br></br>
+        <Thème2/>
+        </span>
+        </div>
+        <br></br>
+
+      </div>
+    
   )
 }
 export default Inspirations
