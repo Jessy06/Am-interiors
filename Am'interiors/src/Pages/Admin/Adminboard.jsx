3298f304586c2422309926a4f32ca3d5591c3849
Jessy06 <91874899+Jessy06@users.noreply.github.com>
2022-06-22 12:23:08 +0200

    Fixed bug Navbar

--- Am'interiors/src/Pages/Admin/Adminboard.jsx
+++ Am'interiors/src/Pages/Admin/Adminboard.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {useCookies} from "react-cookie"
+import {useNavigate} from "react-router-dom"
 
 // import components 
 import NavigationAdmin from '../../components/Admin/Dashboard'
 
 function Dashboard() {
+  const [removeCookies] = useCookies(["token", "pseudo", "id"]);
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    removeCookies('token');
+    removeCookies('pseudo');
+    removeCookies('id');
+
+    navigate("/", {replace: true})
+  };
+
   return (
     <div className="p-4">
       <h1 className='text-8xl text-white text-right border-r-4 border-b-4 border-orange-100/75 p-4'>Dashboard !</h1>
@@ -12,8 +25,10 @@ function Dashboard() {
         <span className="font-title text-xl text-white text-center">AM'in</span>
         terior
         </div>
-
         <NavigationAdmin/>
+      <bouton
+      type="button"
+      onClick = {logOut}> Log out </bouton>
     </div>
   )
 }
