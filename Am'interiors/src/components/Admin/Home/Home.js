4b995337a0eb70661c7680218f3c61b400f02641
Jessy06 <91874899+Jessy06@users.noreply.github.com>
2022-07-07 16:08:40 +0200

    Lien Insta

--- Am'interiors/src/components/Admin/Home/Home.js
+++ Am'interiors/src/components/Admin/Home/Home.js
@@ -16,24 +16,23 @@ function Home() {
         />
         <img
           src={Terrasse}
-          className="border-solid  p-1 lg:bg-black lg:border-solid bg-black"
+          className="border-solid p-1 lg:bg-black lg:border-solid bg-black"
           alt="Terrasse"
         />
         </div>
         <br></br>
         <br></br>
 
-        <div className='grid'>
-      
-        <span className='text-2xl text-black'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis
-          imperdiet quam, eu commodo justo volutpat et. Pellentesque vel
-          tincidunt mauris, porta feugiat odio. Quisque dui tellus, lobortis vel
-          dui id, sollicitudin mollis ligula. Praesent aliquam auctor erat at
-          semper. Cras non euismod odio. Suspendisse et facilisis diam. Mauris a
-          neque tempor, mattis urna vel, tempus sapien. In id quam mattis,
-          tincidunt erat sit amet, hendrerit justo.
-        </span>
+        <div className=''>
+          <span className='text-lg lg:text-2xl text-black'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis
+            imperdiet quam, eu commodo justo volutpat et. Pellentesque vel
+            tincidunt mauris, porta feugiat odio. Quisque dui tellus, lobortis vel
+            dui id, sollicitudin mollis ligula. Praesent aliquam auctor erat at
+            semper. Cras non euismod odio. Suspendisse et facilisis diam. Mauris a
+            neque tempor, mattis urna vel, tempus sapien. In id quam mattis,
+            tincidunt erat sit amet, hendrerit justo.
+          </span>
         </div>
         <br></br>
         </div>
