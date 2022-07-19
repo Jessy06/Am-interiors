a38063b5b9c74214af9c8511830adb52f70c4eb8
JeremieG06 <91867175+JeremieG06@users.noreply.github.com>
2022-07-19 11:28:23 +0200

    updates

--- Am'interiors/src/components/PopUp/Formulaire.js
+++ Am'interiors/src/components/PopUp/Formulaire.js
@@ -49,6 +49,7 @@ export default function Modal() {
         
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
           <div className="relative w-auto my-70 mx-auto max-w-6xl">
+            
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
